e09ce796ab960404bd16947128b84beca96fecd0
Guillaume Maze <gmaze@ifremer.fr>
2026-03-23 14:56:29 +0100

    Update utils.py

--- pydox/dummy/utils.py
+++ pydox/dummy/utils.py
@@ -7,13 +7,14 @@ class Calibration:
     def __init__(self):
         self._cfg = deepcopy(do.params)
 
-    def fit(self):
-        self.set_params('argo.qcflags.psal', 12)
-
     def get_params(self, grp: str):
         """Get configuration parameter(s) for this instance only"""
         return do.get_params(grp, self._cfg)
 
     def set_params(self, grp: str, value: Any | Dict):
         """Set configuration parameter(s) for this instance only"""
-        do.set_params(grp, value, self._cfg)
+        return do.set_params(grp, value, self._cfg)
+
+    def fit(self):
+        self.set_params('argo.qcflags.psal', 12)
+        print("Set internal parameter 'argo.qcflags.psal' to '12'")

91124f4c92297f56e2f051826ffcd4e1464b2a35
C. Kermabon <77444136+CKermabon@users.noreply.github.com>
2026-04-13 16:55:34 +0200

    Update ncep.py

--- pydox/io/inair/ncep/ncep.py
+++ pydox/io/inair/ncep/ncep.py
@@ -2,33 +2,49 @@
 NCEP Module
 """
 import pydox as do
-import glob
+from pathlib import Path
 import xarray as xr
 
-def open_ncep()->xr.dataset.Dataset:
+def open_ncep()->xr.Dataset:
     """
     Opens NCEP files from the NCEP directory defined in the user configuration.
     Returns a dataset xarray with the air/rhum and slp variables.
+
+    This function assumes that the NCEP directory is defined in the user configuration
+     (in calibration_methods.in_air.data.ncep.src).
+     All the NCEP files needed (air.sig995.YYYY.nc, rhum.sig995.YYYY.nc, slp.YYYY.nc) must exist in this directory,
+     at the root level.
     """
-    try:
-        rep_data_ncep = do.get_params('calibration_methods.in_air.data.ncep.src')
-    except:
+
+    src_data_ncep = do.get_params('calibration_methods.in_air.data.ncep.src')
+
+    if not src_data_ncep:
         raise ValueError("NCEP directory not defined")
 
-    if not rep_data_ncep:
-        raise ValueError("NCEP directory not defined")
+    if not Path(src_data_ncep).is_dir():
+        raise ValueError("NCEP directory does not exist")
 
-    fic_ncep = glob.glob(rep_data_ncep + '*nc')
+    files_ncep = [p.resolve() for p in list(Path(src_data_ncep).glob('*.nc'))]
 
-    if not fic_ncep:
-        raise ValueError("No NCEP files found")
+    if len(files_ncep)==0:
+        raise ValueError(f"No NCEP files found in the directory {src_data_ncep}")
 
-    ds_ncep = xr.open_mfdataset(fic_ncep)
+    ds_ncep = xr.open_mfdataset(files_ncep)
 
     needed = ['slp', 'air', 'rhum']
     not_available = [v for v in needed if v not in ds_ncep]
 
     if not_available:
-        raise KeyError(f"No NCEP files availables for : {not_available}")
+        raise ValueError(f"No NCEP files availables for : {not_available} in {src_data_ncep}")
+
+    print('merdum")')
+    print(ds_ncep)
+
+    m1 = ds_ncep['air'].isnull()
+    m2 = ds_ncep['slp'].isnull()
+    m3 = ds_ncep['rhum'].isnull()
+
+    if ((m1 != m2) | (m1 != m3)).any().compute():
+        raise ValueError(f"The 3 variables (air/rhum/slp) don't have the NaN at the same time. Check your NCEP files")
 
     return ds_ncep

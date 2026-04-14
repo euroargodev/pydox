21485ae78afb8ff048251e8643bdd2346e67cc50
C. Kermabon <77444136+CKermabon@users.noreply.github.com>
2026-04-14 11:38:02 +0200

    Update ncep.py
    Add a comment in open_ncep for not forget to check that we have one reference data for each argo data

--- pydox/io/inair/ncep/ncep.py
+++ pydox/io/inair/ncep/ncep.py
@@ -37,7 +37,13 @@ def open_ncep()->xr.Dataset:
     if not_available:
         raise ValueError(f"No NCEP files availables for : {not_available} in {src_data_ncep}")
 
-
+#
+# this takes a long time.
+# Maybe this is not in this file that we must check that we associate a reference data point with each floating-point data point in the fit.
+# We'll have to see where it makes the most sense to do that...
+# For now, we leave it as is
+# .. TO DO ...
+#
     m1 = ds_ncep['air'].isnull()
     m2 = ds_ncep['slp'].isnull()
     m3 = ds_ncep['rhum'].isnull()

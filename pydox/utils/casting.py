6f4b47bdcbd84a3942c3650a06ffc7a4d6c58a44
Guillaume Maze <gmaze@ifremer.fr>
2026-04-29 15:32:46 +0200

    Update casting.py
    new functions

--- pydox/utils/casting.py
+++ pydox/utils/casting.py
@@ -2,4 +2,24 @@ from typing import Any
 
 
 def to_list(x: Any) -> list[Any]:
+    """Convert object to a list"""
     return x if isinstance(x, list) else [x]
+
+
+def ulist(l: list[Any]) -> list[Any]:
+    """Get unique item in list, even if not hashable"""
+    u: list[Any] = []
+    for item in l:
+        if item not in u:
+            u.append(item)
+    return u
+
+
+def is_ulist(l: list[Any]) -> bool:
+    """Has the list only unique values ?"""
+    return len(l) == len(ulist(l))
+
+
+def is_ctelist(l: list[Any]) -> bool:
+    """Has the list a single unique value ?"""
+    return len(ulist(l)) == 1

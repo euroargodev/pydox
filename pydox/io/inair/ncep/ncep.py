345c7748077be1a69c1149dd5278334ef5c36bee
C. Kermabon <77444136+CKermabon@users.noreply.github.com>
2026-05-12 15:08:17 +0200

    Update ncep.py
    C

--- pydox/io/inair/ncep/ncep.py
+++ pydox/io/inair/ncep/ncep.py
@@ -1,6 +1,8 @@
 """
 NCEP Module
 """
+from copy import deepcopy
+
 import pydox as do
 from pathlib import Path
 import xarray as xr
@@ -66,10 +68,9 @@ def interp_NCEP_on_ARGO(ds_ncep: xr.Dataset, coord_argo: dict[str,xr.DataArray])
     Unit conversions are required to calculate NCEP_PPOX
     """
     # Force NCEP lon to be in [-180 180] as the ARGO longitude
-    ds_ncep['lon_dummy'] = xr.where(ds_ncep['lon'] > 180, ds_ncep['lon'] - 360, ds_ncep['lon'])
-
-    ds_ncep_interp = ds_ncep.interp(lat=coord_argo['lat'], lon_dummy=coord_argo['lon'], time=coord_argo['time'])
-    ds_ncep.drop_vars('lon_dummy')
+    ds_ncep_dummy = deepcopy(ds_ncep)
+    ds_ncep_dummy['lon'] = xr.where(ds_ncep_dummy['lon'] > 180, ds_ncep_dummy['lon'] - 360, ds_ncep_dummy['lon'])
+    ds_ncep_interp = ds_ncep_dummy.interp(lat=coord_argo['lat'], lon=coord_argo['lon'], time=coord_argo['time'])
     #
     # Units Conversion
     #

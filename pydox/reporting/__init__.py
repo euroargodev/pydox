b5be679f45bd894febb09278edf17e7538b12afd
Guillaume Maze <gmaze@ifremer.fr>
2026-08-25 18:00:45 +0200

    Update __init__.py

--- pydox/reporting/__init__.py
+++ pydox/reporting/__init__.py
@@ -1,3 +1,12 @@
+"""
+The 'reporting' sub-module is to be understood in a general sense, i.e.:
+- this module is not limited to the 'report' class to generate reporting documents
+- this module includes logging management (for users and dev.)
+- this module includes templating management (eg: config settings, color schemes, jinja templates, ...)
+
+This may need to be more finely refactored in the future.
+
+"""
 from typing import Optional
 from pydox.reporting import colors
 from pydox.reporting import logs

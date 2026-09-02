054fba2acf257903351c3ce788e1dc7349c44552
Guillaume Maze <gmaze@ifremer.fr>
2026-09-02 16:35:14 +0200

    New UnsupportedSetting error

--- pydox/errors.py
+++ pydox/errors.py
@@ -5,3 +5,9 @@ class MissingSetting(ValueError):
     """Raise for missing setting in the configuration"""
 
     pass
+
+
+class UnsupportedSetting(ValueError):
+    """Raise when a setting is given a value that is not supported because of a missing implementation"""
+
+    pass
